4e4c861c824f1f6a883d24a324ed34b24f36c933
unknown <jonesuccess@gmail.com>
2022-06-09 15:22:10 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PayrollAPI.Models;
+using PayrollAPI.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayrollAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService service)
+        {
+            _employeeService = service;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetAllEmployees()
+        {
+            try
+            {
+                var employees = _employeeService.GetEmployeesList();
+                if (employees == null) return NotFound();
+                return Ok(employees);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        [Route("[action]/id")]
+        public IActionResult GetEmployeesById(int id)
+        {
+            try
+            {
+                var employees = _employeeService.GetEmployeeDetailsById(id);
+                if (employees == null) return NotFound();
+                return Ok(employees);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SaveEmployees(Employees employeeModel)
+        {
+            try
+            {
+                var model = _employeeService.SaveEmployee(employeeModel);
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            try
+            {
+                var model = _employeeService.DeleteEmployee(id);
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
